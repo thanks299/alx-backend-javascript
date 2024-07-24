4a6309a9419ac613fdc5b013fabb1a9e6dd64606
thanks299 <thanksagbeble@gmail.com>
2024-07-24 12:18:58 -0700

    basic

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable */
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-      income,
-      gdp,
-      capita,
-    };
-  
-    return budget;
-  }
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
